542889438e9a3eeab66dd9f2a20219010f6a6c50
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-05-18 18:22:06 -0300

    Requisito obrigatório 6

--- src/challenges.js
+++ src/challenges.js
@@ -41,24 +41,54 @@ function footballPoints(wins, ties) {
 console.log(footballPoints(0,0));
 
 // Desafio 6
-function highestCount() {
- /*let array = [];
+function highestCount(array) {
+  let maxValue = array[0];
+  for (let index = 0; index < array.length; index += 1) {
+    if(array[index] > maxValue) {
+      maxValue = array[index];
+    }
+  }
   let sum = 0;
-  for (let index = 0; index < array.length; index += 1) 
-    if (array)
-  }*/
-  
+  for (let secondIndex = 0; secondIndex < array.length; secondIndex += 1) {
+    if (maxValue == array[secondIndex]) {
+      sum += 1;
+    }
+  }
+  return sum;
 }
+
+console.log(highestCount([0, 0, 0]));
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+ /* if (cat1 - mouse < cat2 - mouse) {
+    return 'cat1';
+  } else if (mouse - cat1) 
+  else if (cat2 - mouse < cat1 - mouse ) {
+    return 'cat2';
+  } else if (cat1 = cat2) {
+    return 'os gatos trombam e o rato foge';
+  } */
 }
 
+//console.log(catAndMouse(2,3,5));
+
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+/*  for (index = 0; index < array.length; index += 1) {
+    if ((array[index] / 3) == true) {
+      return 'fizz';
+    } else if ((array[index] / 5) == true) {
+      return 'buzz';
+    } else if ((array[index] / 3) && (array[index] / 5) == true) {
+    return 'fizzBuzz';
+    } else if ((array[index] / 3) && (array[index] / 5) == false) {
+      return 'bug!';
+    }
+  } */
 }
+
+//console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
 function encode() {
