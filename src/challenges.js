e496ff037800b128b41b8cf08788e52899ad88cc
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-12-20 15:34:49 -0300

    Requisito 11

--- src/challenges.js
+++ src/challenges.js
@@ -15,16 +15,16 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(string) {
-  let result = string.split(' ', 9);
+  let result = string.split(' ');
   return result;
 }
 
-console.log(splitSentence('Foguete'));
+console.log(splitSentence('foguete'));
 
 // Desafio 4
 
 function concatName(string) {
-  let array = string[string.length-1] + ", " + string[0];
+  let array = string[string.length - 1] + ", " + string[0];
   return array;
 }
 
@@ -38,111 +38,113 @@ function footballPoints(wins, ties) {
   return sum;
 }
 
-console.log(footballPoints(0,0));
+console.log(footballPoints(14, 8));
 
 // Desafio 6
 function highestCount(array) {
   let maxValue = array[0];
   for (let index = 0; index < array.length; index += 1) {
-    if(array[index] > maxValue) {
+    if (array[index] > maxValue) {
       maxValue = array[index];
+      return maxValue;
     }
   }
   let sum = 0;
   for (let secondIndex = 0; secondIndex < array.length; secondIndex += 1) {
-    if (maxValue == array[secondIndex]) {
+    if (maxValue === array[secondIndex]) {
       sum += 1;
     }
   }
   return sum;
 }
 
-console.log(highestCount([0, 0, 0]));
+console.log(highestCount([3, 4, 3, 5, 7, 3, 8, 3]));
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let catMouse1 = Math.abs(cat1 - mouse);
   let catMouse2 = Math.abs(cat2 - mouse);
-  
+
   if (catMouse1 < catMouse2) {
     return 'cat1';
   } else if (catMouse2 < catMouse1) {
     return 'cat2';
-  } else if (catMouse1 == catMouse2) {
+  } else if (catMouse1 === catMouse2) {
     return 'os gatos trombam e o rato foge';
   }
 }
 
 // Usei Math.abs() para resolver essa questão.. encontrei essa solução neste link: https://www.w3schools.com/jsref/jsref_abs.asp
 
-console.log(catAndMouse(12,6,5));
+console.log(catAndMouse(12, 6, 6));
 
 // Desafio 8
 function fizzBuzz2(array) {
-  if (array[index] % 3 == 0 && array[index] % 5 == 0) {
+  if (array[index] % 3 === 0 && array[index] % 5 === 0) {
     return 'fizzBuzz';
-  } else if (array[index] % 5 == 0 ) {
+  } else if (array[index] % 5 === 0) {
     return 'buzz';
-  } else if (array[index] % 3 == 0) {
-  return 'fizz';
-  } else if (array[index] % 3 != 0 && array[index] % 5 != 0) {
+  } else if (array[index] % 3 === 0) {
+    return 'fizz';
+  } else if (array[index] % 3 !== 0 && array[index] % 5 !== 0) {
     return 'bug!';
   }
-}
+
+};
 
 function fizzBuzz(array) {
   let array2 = [];
-for (index = 0; index < array.length; index += 1) {
+  for (index = 0; index < array.length; index += 1) {
     array2.push(fizzBuzz2(array));
   }
-  return array2; 
-}
+  return array2;
+};
 
-console.log(fizzBuzz([9, 25]));
+console.log(fizzBuzz([9, 25, 15, 7]));
 
 // Desafio 9
 
 function encode(string) {
   let splitString = string.split('');
   for (index = 0; index < splitString.length; index += 1) {
-    if (splitString[index] == 'a') { 
+    if (splitString[index] === 'a') {
       splitString[index] = 1;
-  } else if (splitString[index] == 'e') {
-    splitString[index] = 2;
-  } else if (splitString[index] == 'i') {
-    splitString[index] = 3;
-  } else if (splitString[index] == 'o') {
-    splitString[index] = 4;
-  } else if (splitString[index] == 'u') {
-    splitString[index] = 5;
-  }
+    } else if (splitString[index] === 'e') {
+      splitString[index] = 2;
+    } else if (splitString[index] === 'i') {
+      splitString[index] = 3;
+    } else if (splitString[index] === 'o') {
+      splitString[index] = 4;
+    } else if (splitString[index] === 'u') {
+      splitString[index] = 5;
+    }
   }
   let joinString = splitString.join('');
   return joinString;
 }
-  
-  console.log(encode('go Trybe!'));
+
+console.log(encode('go Trybe!'));
 
 function decode(string) {
   let splitString = string.split('');
   for (index = 0; index < splitString.length; index += 1) {
-    if (splitString[index] == 1) { 
+    if (splitString[index] === 1) {
       splitString[index] = 'a';
-  } else if (splitString[index] == 2) {
-    splitString[index] = 'e';
-  } else if (splitString[index] == 3) {
-    splitString[index] = 'i';
-  } else if (splitString[index] == 4) {
-    splitString[index] = 'o';
-  } else if (splitString[index] == 5) {
-    splitString[index] = 'u';
-  }
+    } else if (splitString[index] === 2) {
+      splitString[index] = 'e';
+    } else if (splitString[index] === 3) {
+      splitString[index] = 'i';
+    } else if (splitString[index] === 4) {
+      splitString[index] = 'o';
+    } else if (splitString[index] === 5) {
+      splitString[index] = 'u';
+    }
   }
   let joinString = splitString.join('');
-  return joinString;  
+  return joinString;
 }
 
-  console.log(decode('h3 th2r2!'));
+console.log(decode('h3 th2r2!'));
 
 // Desafio 10
 function techList(array, string) {
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,27 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let sum = 0;
+
+  for (index = 0; index < array.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < array.length; secondIndex += 1) {
+      if (array[index] === array[secondIndex]) {
+        sum += 1;
+      }
+
+      if (array.length !== 11) {
+        return "Array com tamanho incorreto.";
+      } else if (array[index] < 0 || array[index] > 9 || sum >= 3) {
+        return "não é possível gerar um número de telefone com esses valores";
+      }
+    }
+    array.splice(0, 0, '(',);
+    array.splice(3, 0, ')',);
+    array.splice(9, 0, '-',);
+
+    return array.join('');
+  }
 }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
